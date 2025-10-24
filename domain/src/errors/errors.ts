8607817521e2f421bb02c4ece9b796b002ebe703
Lucas Villanueva <lucasvillanueva.dev@gmail.com>
2025-10-24 01:04:37 +0000

    feat: add custom error classes for improved error handling

---
+++ domain/src/errors/errors.ts
@@ -0,0 +1,34 @@
+export class PasswordValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "PasswordValidationError";
+  }
+}
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ConflictError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ConflictError";
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor(message = "Unauthorized") {
+    super(message);
+    this.name = "UnauthorizedError";
+  }
+}
