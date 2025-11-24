e8e65c0922cf79fd473aa29c23f1aaff51ddd7e1
Lucas Villanueva <lucasvillanueva.dev@gmail.com>
2025-11-24 17:15:58 +0000

    feat: add image configuration for remote patterns in Next.js

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.igdb.com",
+        port: "",
+        pathname: "/igdb/image/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
